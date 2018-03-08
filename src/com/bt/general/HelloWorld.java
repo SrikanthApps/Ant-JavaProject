eb4a9cb4e602c355778948eaae3f5bb12de7d15f
SrikanthNani <36352815+SrikanthNani@users.noreply.github.com>
2018-03-08 11:06:26 +0530

    Update HelloWorld.java

--- src/com/bt/general/HelloWorld.java
+++ src/com/bt/general/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to ANT !! " + getLocalCurrentDate());
-		System.out.println("This is srikanth");
+		System.out.println("This is SrikanthNani");
 	}
 
 	private static Date getLocalCurrentDate() {
